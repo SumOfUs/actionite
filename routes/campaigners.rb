aed7384d2e5353e36388950907a92b4b1f54a2bb
Eric Boersma <eric.boersma@gmail.com>
2015-03-10 12:29:06 -0400

    Adding petitions routes

--- routes/campaigners.rb
+++ routes/campaigners.rb
@@ -19,6 +19,19 @@ class Campaigners < Cuba
       res.redirect "/"
     end
 
+    on "petitions/new" do
+      render "petition/new", title: "Actionite | Create New Petition", petition: Petition.new, form: partial('petition/form')
+    end
+
+    on "petitions/edit/:slug" do |slug|
+      petition = Petition.find_by_slug slug
+      render "petition/edit", title: "Actionite | Create New Petition", petition: petition
+    end
+
+    on "petitions" do
+      render "petition/index", title: "Actionite | Petitions"
+    end
+
     on(default) { not_found! }
   end
 end
